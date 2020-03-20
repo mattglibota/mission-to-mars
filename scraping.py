48a9caf99db7c6db84d1941cd378ac0484a27386
mattglibota <59847215+mattglibota@users.noreply.github.com>
2020-03-19 19:54:07 -0700

    flask building

--- scraping.py
+++ scraping.py
@@ -2,14 +2,30 @@
 # Import dependencies
 from splinter import Browser
 from bs4 import BeautifulSoup
-import pandas as pd
+import pandas as pd, datetime as dt
 
 def scrape_all():
-    # Set the executable path and initialize the chrome browser in splinter
-    executable_path = {'executable_path': 'chromedriver'}
-    browser = Browser('chrome', **executable_path)
+    # Initiate headless driver for deployment
+    browser = Browser("chrome", executable_path="chromedriver", headless=True)
+
+    # run mars news
+    news_title, news_paragraph = mars_news(browser)
+    
+    #run all scraping functions and store data
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
 
 
+# Set the executable path and initialize the chrome browser in splinter
+executable_path = {'executable_path': 'chromedriver'}
+browser = Browser('chrome', **executable_path)
+
+def mars_news():
     # ### Mars News
 
     # Visit the mars nasa news site
@@ -20,17 +36,23 @@ def scrape_all():
 
     html = browser.html
     news_soup = BeautifulSoup(html,'html.parser')
-    slide_elem = news_soup.select_one('ul.item_list li.slide')
 
-    # Use the parent element to find the first `a` tag and save it as `news_title`
-    news_title = slide_elem.find("div", class_='content_title').get_text()
-    news_title
+    try:
+        # find element we want
+        slide_elem = news_soup.select_one('ul.item_list li.slide')
 
-    # Use the parent element to find the paragraph text
-    news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
-    news_p
+        # Use the parent element to find the first `a` tag and save it as `news_title`
+        news_title = slide_elem.find("div", class_='content_title').get_text()
 
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
+    
+    except AttributeError:
+        return None, None
 
+    return news_title, news_p
+
+def featured_image(browser):
     # ### Featured Images
 
     # Visit URL
@@ -50,22 +72,34 @@ def scrape_all():
     html = browser.html
     img_soup = BeautifulSoup(html, 'html.parser')
 
-    img_url_rel = img_soup.select_one('figure.lede a img').get('src')
-    img_url_rel
+    try:
+        img_url_rel = img_soup.select_one('figure.lede a img').get('src')
+    
+    except AttributeError:
+        return None
 
     # make a clickable url
     img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
-    img_url
+    
+    return img_url
 
-
+def mars_facts():
     # ### Space Facts
-
+    try:
     # get html via pandas
-    df = pd.read_html('http://space-facts.com/mars/')[0]
+        df = pd.read_html('http://space-facts.com/mars/')[0]
+
+    except BaseException:
+        return None
+
     df.columns = ['description', 'value']
     df.set_index('description', inplace=True)
 
-    mars = df.to_html()
+    return df.to_html()
 
-    browser.quit()
+browser.quit()
+
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())    
 
